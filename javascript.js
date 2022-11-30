d877b486d1990093ff42940731e722c7910c645d
barnettalex99 <alexgbarnett@gmail.com>
2022-11-30 00:22:54 -0500

    javascript working grid with hover method

---
+++ javascript.js
@@ -0,0 +1,25 @@
+/*This code allows the active drawing screen to take mouse input to draw within the container by changing the color of the pixels.
+It will also include a "clear" method that will allow the grid to be reset to all white.
+Pseudocode:
+create grid using HTML and CSS flexbox
+have grid square color change to black after hover
+have it stay black after done hovering to maintain shapes
+clear button turns all of the grid parts back to white */
+
+const gridContainer = document.querySelector(".screen-container");
+const clearButton = document.querySelector(".button");
+
+function createGrid(rows, columns){
+    gridContainer.style.setProperty('--grid-rows', rows);
+    gridContainer.style.setProperty('--grid-cols', columns);
+    for (i = 0; i < (rows * columns); i++){
+        let cell = document.createElement("div");
+        cell.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "black"
+        })
+        gridContainer.appendChild(cell).className = "grid-item";
+    }
+    
+}
+
+createGrid(16,16);
